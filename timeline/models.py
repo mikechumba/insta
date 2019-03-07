f9b01dedad76753672771d2b9f2814f4c987d382
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-07 15:52:36 +0300

    create profile model

--- timeline/models.py
+++ timeline/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+
+   name = models.CharField(max_length=50)
+   username = models.CharField(max_length=50)
+   email = models.EmailField(max_length=254)
+   avatar = models.ImageField(upload_to='avatar/', height_field=None, width_field=None, max_length=None)
+   bio = models.TextField(max_length=140)
+
+   def __str__(self):
+      return self.name
+   
