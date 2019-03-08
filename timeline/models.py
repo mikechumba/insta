b47f8bac444dcbc2b49485cb0a4f4145924c8490
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-08 14:27:14 +0300

    update profile model to extend user model

--- timeline/models.py
+++ timeline/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
 
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
    name = models.CharField(max_length=50)
-   username = models.CharField(max_length=50)
    email = models.EmailField(max_length=254)
    avatar = models.ImageField(upload_to='avatar/', height_field=None, width_field=None, max_length=None)
    bio = models.TextField(max_length=140)
@@ -17,10 +18,23 @@ class Image(models.Model):
    img = models.ImageField(upload_to='', height_field=None, width_field=None, max_length=None)
    caption = models.TextField(max_length=280)
    img_name = models.CharField(max_length=50)
-   likes = models.ForeignKey(Profile, on_delete=models.CASCADE)
+   author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.caption
 
 class Comments(models.Model):
    
    comment = models.TextField(max_length=140)
+   user = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
-   
+   def __str__(self):
+      return self.comment
+
+class Follow(models.Model):
+
+   followers = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followed_by')
+   following = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+
+   def __str__(self):
+      return self.followers
